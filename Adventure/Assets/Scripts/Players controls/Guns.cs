f3b178cf8c2941b5da8b03bc5411b6c7605c0dbc
blaz Ocepek <bocepek027@gmail.com>
2017-12-07 00:59:48 +0100

    Premik ladnje in guna
    ustvaril projekt,naredil ladjo ki se premika, premika se gun proti miski, ustavril bullet za iztrelit (todo)

---
+++ Adventure/Assets/Scripts/Players controls/Guns.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guns : MonoBehaviour {
+
+    public GameObject muzzle;
+    private float lastShot;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lookatMouse();
+        shoot();
+	}
+
+    void shoot()
+    {
+        lastShot += Time.deltaTime;
+        if (Input.GetMouseButton(0) && lastShot >= 2)
+        {
+            lastShot = 0;
+            //shoot bullet or what ever
+            Debug.Log("SHOOT");
+        }
+    }
+
+    void lookatMouse()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float AngleRad = Mathf.Atan2(mousePos.y - transform.position.y, mousePos.x - transform.position.x);
+        float AngleDeg = (180 / Mathf.PI) * AngleRad;
+        transform.rotation = Quaternion.Euler(0, 0, AngleDeg);
+    }
+
+
+}
---
+++ Adventure/Assets/Scripts/Players controls/PlayerMovement.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public GameObject ForwardThruster;
+    public GameObject BackwerdsThruster;
+    public GameObject UpThruster;
+    public GameObject DownThruster;
+
+
+
+    public Vector3 speed;
+	// Use this for initialization
+	void Start () {
+        speed = new Vector2(0f, 0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        getInputAndParticle();
+        movePLayer();
+	}
+
+    void getInputAndParticle()
+    {
+        //Forward
+        if (Input.GetKey(KeyCode.D))
+        {
+            speed += new Vector3(0.01f, 0, 0);
+            ForwardThruster.GetComponent<ParticleSystem>().Play();
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            ForwardThruster.GetComponent<ParticleSystem>().Stop();
+        }
+
+
+        //UP
+        if (Input.GetKey(KeyCode.W))
+        {
+            speed += new Vector3(0, 0.01f, 0);
+            UpThruster.GetComponent<ParticleSystem>().Play();
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            UpThruster.GetComponent<ParticleSystem>().Stop();
+        }
+
+        //Down
+        if (Input.GetKey(KeyCode.A))
+        {
+            speed += new Vector3(-0.01f, 0, 0);
+            BackwerdsThruster.GetComponent<ParticleSystem>().Play();
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            BackwerdsThruster.GetComponent<ParticleSystem>().Stop();
+        }
+
+
+        //Backwerds
+        if (Input.GetKey(KeyCode.S))
+        {
+            speed += new Vector3(0, -0.01f, 0);
+            DownThruster.GetComponent<ParticleSystem>().Play();
+        }
+        if(Input.GetKeyUp(KeyCode.S))
+        {
+            DownThruster.GetComponent<ParticleSystem>().Stop();
+        }
+    }
+
+    void movePLayer ()
+    {
+        transform.position += speed * Time.deltaTime;
+    }
+}
